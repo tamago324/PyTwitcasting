6bf48e3d62d608b00a276a32a534c2a43eae33c5
tamago324 <tamago_pad@yahoo.co.jp>
2018-06-29 23:11:45 +0900

    Use vcrpy
    vcrpyを使ってみた
    これで、レスポンスを保存しておき、毎回同じ結果を取得できるようになる

---
+++ tests/test_user.py
@@ -0,0 +1,53 @@
+import os
+import pytest
+import vcr
+
+from pytwitcasting.api import API
+from pytwitcasting.models import User
+
+TOKEN = os.environ['ACCESS_TOKEN']
+
+# URLとリクエストメソッドが一致するリクエストが同一とみなされる
+tape = vcr.VCR(
+    serializer='json',
+    cassette_library_dir='tests/cassettes',
+    record_mode='once',
+    # アクセストークンを消去する
+    filter_headers=['Authorization']
+)
+
+
+"""
+. <- ここで以下のコマンドを実行する
+├── pytwitcasting
+│   ├── api.py
+│   ├── auth.py
+│   ├── error.py
+│   ├── models.py
+│   ├── parsers.py
+│   └── utils.py
+└── tests
+    └── test_userparse.py
+
+$ pipenv run python3 -m pytest
+"""
+
+
+@pytest.fixture
+def user_key():
+    return ['id', 'screen_id', 'name', 'image', 'profile', 'level',
+            'last_movie_id', 'is_live', 'supporter_count',
+            'supporting_count', 'created']
+
+
+@tape.use_cassette('tests/vcr_cassettes/user_info.json')
+def test_get_user_info(user_key):
+    api = API(auth=TOKEN)
+
+    user = api.get_user_info('tamago324_pad')
+
+    assert isinstance(user, User)
+    assert user.screen_id == 'tamago324_pad'
+
+    for k in user_key:
+        assert hasattr(user, k)
